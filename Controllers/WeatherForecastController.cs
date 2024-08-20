16b76fd873a90612726c95750d89962a9a45b8eb
Agilsys <agilsyssoftware@gmail.com>
2024-08-20 11:25:48 +0530

    commit

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -10,6 +10,7 @@ namespace SafeStashAdminPortalAPI.Controllers
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            //
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
